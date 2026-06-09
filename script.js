2e613b5df3b6d7aa37c8287bd529127e4664c0e1
HEMA-NAGARAJ <backiyahema12@gmail.com>
2026-06-09 11:51:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ function generateResume() {
     let phone = document.getElementById("phone").value;
     let skills = document.getElementById("skills").value;
     let education = document.getElementById("education").value;
-
     document.getElementById("resume").innerHTML = `
-        <h2>${name}</h2>
-        <p>Email: ${email}</p>
-        <p>Phone: ${phone}</p>
-        <p>Skills: ${skills}</p>
-        <p>Education: ${education}</p>
-    `;
+    <h2>${name}</h2>
+    <p>Email: ${email}</p>
+    <p>Phone: ${phone}</p>
+    <p>Skills: ${skills}</p>
+    <p>Education: ${education}</p>
+`;
 }
